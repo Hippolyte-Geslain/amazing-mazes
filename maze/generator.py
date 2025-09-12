226862cde3f1291c7d6195d7d87086b60aa7b80a
hippo <hippolyte.geslain@laplateforme.io>
2025-09-12 11:01:32 +0200

    finished backtracking generator

--- maze/generator.py
+++ maze/generator.py
@@ -1,53 +1,69 @@
 import random
 
-directions = {
-    (0,1),
-    (0,-1),
-    (-1,0),
-    (1,0)
-}
+directions = [
+    (0, 2),   
+    (0, -2),  
+    (2, 0),   
+    (-2, 0)   
+]
 
 def ask_user_size():
     size = int(input("what size is the maze ?"))
+    if size % 2 == 0:
+        size+= 1
     return size
 
 def ask_user_name():
     name = input("what is the name of the maze ?")
-    return name+".txt"
+    return name + ".txt"
 
 class Grid():
-    def __init__(self, size, name):
+    def __init__(self):
         self.size = ask_user_size()
         self.name = ask_user_name()
         self.wall = "#"
         self.path = "."
         self.grid = []
         self.stack = []
-        self.neighbors = []
-        self.visited = []
+        self.visited = set()
 
     def create_grid(self):
-        self.grid = [[self.wall for _ in range(self.size)]for _ in range(self.size)]
-        with open(self.name,'w') as f:
+        self.grid = [[self.wall for _ in range(self.size)] for _ in range(self.size)]
+        self.create_path_backtrack((1, 1))
+        self.grid[0][1] = self.path
+        self.grid[self.size-1][self.size-2] = self.path
+        with open(self.name, 'w') as f:
             for line in self.grid:
-                f.write("".join(line)+"\n")
+                f.write("".join(line) + "\n")
 
-
-    def create_path_backtrack(self,cell):
-        start = (1,1)
+    def create_path_backtrack(self, cell):
+        x, y = cell
+        self.grid[x][y] = self.path
         self.stack.append(cell)
-
+        self.visited.add(cell)
 
         while self.stack:
-            x,y = self.stack[-1]
-            self.neighbors = self.find_non_visited(x,y)
-            if self.neighbors:
-                nx,ny = random.choice(self.neighbors)
+            x, y = self.stack[-1]
+            neighbors = self.find_non_visited(x, y)
+            if neighbors:
+                nx, ny = random.choice(neighbors)
+                wall_x, wall_y = (x + nx) // 2, (y + ny) // 2
+                self.grid[wall_x][wall_y] = self.path
                 self.grid[nx][ny] = self.path
+                self.stack.append((nx, ny))
+                self.visited.add((nx, ny))
+            else:
+                self.stack.pop()
 
-    def find_non_visited(self,x,y,):
-        for (dx,dy) in directions:
-            nx, ny = x + dx, dy + y
-            if 0<=nx<self.size and 0<=ny<self.size:
-                if nx and ny not in self.stack:
-                    self.neighbors.append((nx,ny))
+    def find_non_visited(self, x, y):
+        neighbors = []
+        for dx, dy in directions:
+            nx, ny = x + dx, y + dy
+            if 1 <= nx < self.size - 1 and 1 <= ny < self.size - 1:
+                if (nx, ny) not in self.visited:
+                    neighbors.append((nx, ny))
+        return neighbors
+
+if __name__ == "__main__":
+    maze = Grid()
+    maze.create_grid()
